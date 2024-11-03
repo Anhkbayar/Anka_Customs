83f43972cd023d22402f19be4f7008b06d265900
Ankhbayar <zanhaa56@gmail.com>
2024-11-03 18:59:32 +0800

    BOOM!

---
+++ FrontEnd/test.js
@@ -0,0 +1,24 @@
+const nav = document.querySelector('#header_container');
+const footer = document.querySelector('#footer_container');
+const div = document.querySelector('#product_box');
+fetch('Header.html')
+  .then(res => res.text())
+  .then(data => {
+    nav.innerHTML = data;
+  });
+fetch('Footer.html')
+  .then(res => res.text())
+  .then(data =>{
+    footer.innerHTML = data;
+  });
+fetch('Productbox.html')
+  .then(res => res.text())
+  .then(data =>{
+    div.innerHTML = data;
+  })
+fetch('http://localhost:3000')
+.then(res => res.text())
+.then(data =>{
+  console.log(data)
+
+});
