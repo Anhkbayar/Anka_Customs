7f5b77cc1be8b9287a8b1143834f3d1dd3179ccc
huslee-18 <146917917+huslee-18@users.noreply.github.com>
2024-11-08 11:01:42 +0800

    Add files via upload

---
+++ FrontEnd/ForgotPassword.js
@@ -0,0 +1,30 @@
+const nav = document.querySelector('#header_container');
+const footer = document.querySelector('#footer_container');
+fetch('Header.html')
+  .then(res => res.text())
+  .then(data => {
+    nav.innerHTML = data;
+  });
+fetch('Footer.html')
+  .then(res => res.text())
+  .then(data =>{
+    footer.innerHTML = data;
+  });
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const data = new FormData(event.target);
+
+    const value = Object.fromEntries(data.entries());
+
+    fetch("http://localhost:3000/ForgotPassword", {
+        method: "POST",
+        body: JSON.stringify(value),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      });
+}
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
+
