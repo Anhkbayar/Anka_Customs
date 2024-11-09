8375e37d63a58da51505641d9ee1a0d2264e85d7
Ankhbayar <zanhaa56@gmail.com>
2024-11-09 20:53:30 +0800

    idk

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 const connect = require('./db')
-const userRouter = require("./routes/userRoute");
-const bodyParser = require('body-parser');
+const path = require('path')
+const userRouter = require("./routes/userRoute")
+const bodyParser = require('body-parser')
 
-const app = express();
-const port = 3000;
-connect();
+const app = express()
+const port = 3000
+connect()
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use("/", userRouter);
+app.use(bodyParser.json())
+app.use("/", userRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
